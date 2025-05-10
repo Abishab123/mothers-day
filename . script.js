9706ff3f250b2bebde5385ec181e0c23a4eb3432
Abishab123 <bhandariabisha40@gmail.com>
2025-05-10 13:08:47 -0400

    Create . script.js

---
+++ . script.js
@@ -0,0 +1,4 @@
+// The JavaScript file to handle interactions (if needed in the future)
+document.getElementById('checkbox').addEventListener('change', function() {
+    // This handles the checkbox state change for the letter animation
+});
